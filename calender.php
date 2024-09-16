d1e5715ff44ad3a76dd9f93a367bd0695bbcd6e3
Ever-galarza <Evergalarza30@gmail.com>
2024-09-16 00:02:55 -0500

    h

--- calender.php
+++ calender.php
@@ -12,31 +12,31 @@
        $dayofweek = date("w");
        
        switch ($dayofweek) {
-          case 1; 
+          case 2; 
             echo "<p>It is Monday!</p>";
             break;
 
-        case 2; 
+        case 3; 
             echo "<p>It is Tuesday!</p>";
             break;
 
-        case 3; 
+        case 4; 
             echo "<p>It is Wednesday!</p>";
             break;
 
-        case 4; 
+        case 5; 
             echo "<p>It is Thursday!</p>";
             break;
 
-        case 5; 
+        case 6; 
             echo "<p>It is Friday!</p>";
             break;
 
-        case 6; 
+        case 7; 
             echo "<p>It is Saturday!</p>";
             break;
 
-        case 0; 
+        case 1; 
             echo "<p>It is Sunday!</p>";
             break;
 
