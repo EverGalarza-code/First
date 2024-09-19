6277662655814cfcdb40423fcbb57397c6a24d73
Ever-galarza <Evergalarza30@gmail.com>
2024-09-19 00:36:12 -0500

    '
    '

--- index.php
+++ index.php
@@ -8,12 +8,22 @@ include 'includes/header.php';
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Coding II Test</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <h1>Hello, world!</h1>
+
+    <form method="GET">
+        <input type="text" name="person">
+        <button>SUBMIT</button>
+    </form>
+
+    <?php
+        $name = $_GET['person'];
+        echo $name."This is actually very simple";
+    ?>
     
   </body>
 </html>
